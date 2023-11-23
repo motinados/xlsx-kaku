2bf79f103461267a87e9fd48ba09db0c41acd66e
motinados <motinados@gmail.com>
2023-11-23 15:56:15 +0900

    findLastNonNullCell

--- src/writer.ts
+++ src/writer.ts
@@ -12,3 +12,19 @@ export function findFirstNonNullCell(row: NullableCell[]) {
   }
   return { firstNonNullCell, index };
 }
+
+/**
+ *  [null, null, null, nonnull, null] => index is 3
+ */
+export function findLastNonNullCell(row: NullableCell[]) {
+  let index = -1;
+  let lastNonNullCell = null;
+  for (let i = row.length - 1; i >= 0; i--) {
+    if (row[i] !== null) {
+      lastNonNullCell = row[i]!;
+      index = i;
+      break;
+    }
+  }
+  return { lastNonNullCell, index };
+}
--- tests/writer.test.ts
+++ tests/writer.test.ts
@@ -1,5 +1,5 @@
 import { NullableCell } from "../src/cell";
-import { findFirstNonNullCell } from "../src/writer";
+import { findFirstNonNullCell, findLastNonNullCell } from "../src/writer";
 
 describe("Writer", () => {
   test("findFirstNonNullCell", () => {
@@ -12,5 +12,17 @@ describe("Writer", () => {
     const { firstNonNullCell, index } = findFirstNonNullCell(row);
     expect(firstNonNullCell).toEqual({ type: "string", value: "name" });
     expect(index).toBe(2);
+  });
+
+  test("findLastNonNullCell", () => {
+    const row: NullableCell[] = [
+      null,
+      null,
+      { type: "string", value: "name" },
+      { type: "string", value: "age" },
+    ];
+    const { lastNonNullCell, index } = findLastNonNullCell(row);
+    expect(lastNonNullCell).toEqual({ type: "string", value: "age" });
+    expect(index).toBe(3);
   });
 });

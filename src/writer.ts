506fe90ba0d8a1cff8a7fe36eb90e19a44fa58ae
motinados <motinados@gmail.com>
2023-11-25 04:44:15 +0900

    writer for number cell

--- src/writer.ts
+++ src/writer.ts
@@ -1,4 +1,4 @@
-import { NullableCell } from "./cell";
+import { NullableCell, convNumberToColumn } from "./cell";
 
 export function findFirstNonNullCell(row: NullableCell[]) {
   let index = -1;
@@ -27,4 +27,52 @@ export function findLastNonNullCell(row: NullableCell[]) {
     }
   }
   return { lastNonNullCell, index };
+}
+
+/**
+ * <row r="1" spans="1:2"><c r="A1" t="s"><v>0</v></c><c r="B1" t="s"><v>1</v></c></row>
+ */
+export function rowToString(row: NullableCell[], rowIndex: number) {
+  const first = findFirstNonNullCell(row);
+  if (first === undefined || first === null) {
+    throw new Error("row is empty");
+  }
+
+  const last = findLastNonNullCell(row);
+  if (last === undefined) {
+    throw new Error("row is empty");
+  }
+
+  const startSpan = first.index;
+  const lastSpan = last.index;
+
+  let result = `<row r="${rowIndex}" spans="${startSpan}:${lastSpan}">`;
+
+  let columnIndex = 0;
+  for (const cell of row) {
+    if (cell !== null) {
+      result += cellToString(cell, columnIndex, rowIndex);
+    }
+
+    columnIndex++;
+  }
+
+  result += `</row>`;
+  return result;
+}
+
+export function cellToString(
+  cell: NonNullable<NullableCell>,
+  columnIndex: number,
+  rowIndex: number
+) {
+  const column = convNumberToColumn(columnIndex);
+  switch (cell.type) {
+    case "number": {
+      return `<c r="${column}${rowIndex}"><v>${cell.value}</v></c>`;
+    }
+    default: {
+      throw new Error(`not implemented: ${cell.type}`);
+    }
+  }
 }
--- tests/writer.test.ts
+++ tests/writer.test.ts
@@ -1,5 +1,10 @@
 import { NullableCell } from "../src/cell";
-import { findFirstNonNullCell, findLastNonNullCell } from "../src/writer";
+import {
+  cellToString,
+  findFirstNonNullCell,
+  findLastNonNullCell,
+  rowToString,
+} from "../src/writer";
 
 describe("Writer", () => {
   test("findFirstNonNullCell", () => {
@@ -24,5 +29,27 @@ describe("Writer", () => {
     const { lastNonNullCell, index } = findLastNonNullCell(row);
     expect(lastNonNullCell).toEqual({ type: "string", value: "age" });
     expect(index).toBe(3);
+  });
+
+  test("cellToString", () => {
+    const cell: NonNullable<NullableCell> = {
+      type: "number",
+      value: 15,
+    };
+    const result = cellToString(cell, 2, 1);
+    expect(result).toBe(`<c r="C1"><v>15</v></c>`);
+  });
+
+  test("rowToString", () => {
+    const row: NullableCell[] = [
+      null,
+      null,
+      { type: "number", value: 15 },
+      { type: "number", value: 23 },
+    ];
+    const result = rowToString(row, 1);
+    expect(result).toBe(
+      `<row r="1" spans="2:3"><c r="C1"><v>15</v></c><c r="D1"><v>23</v></c></row>`
+    );
   });
 });

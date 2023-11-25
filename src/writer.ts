bb5ae37fb39f247648877120804d0f4d59e17ad4
motinados <motinados@gmail.com>
2023-11-25 15:25:52 +0900

    makeStylesXml

--- src/writer.ts
+++ src/writer.ts
@@ -28,6 +28,9 @@ export function writeFile(filename: string) {
     fs.mkdirSync(xlPath, { recursive: true });
   }
 
+  const stylesXml = makeStylesXml();
+  fs.writeFileSync(path.join(xlPath, "styles.xml"), stylesXml);
+
   const xl_relsPath = path.resolve(xlPath, "_rels");
   if (!fs.existsSync(xl_relsPath)) {
     fs.mkdirSync(xl_relsPath, { recursive: true });
@@ -258,5 +261,56 @@ function makeCoreXml() {
   );
   results.push("<cp:contentStatus></cp:contentStatus>");
   results.push("</cp:coreProperties>");
+  return results.join("");
+}
+
+function makeStylesXml() {
+  const results: string[] = [];
+  results.push('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>');
+  results.push(
+    '<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac x16r2 xr" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:x16r2="http://schemas.microsoft.com/office/spreadsheetml/2015/02/main" xmlns:xr="http://schemas.microsoft.com/office/spreadsheetml/2014/revision">'
+  );
+  results.push('<fonts count="1">');
+  results.push("<font>");
+  results.push('<sz val="11"/>');
+  results.push('<color theme="1"/>');
+  results.push('<name val="Calibri"/>');
+  results.push('<family val="2"/>');
+  results.push('<scheme val="minor"/></font>');
+  results.push("</fonts>");
+  results.push('<fills count="2">');
+  results.push('<fill><patternFill patternType="none"/></fill>');
+  results.push('<fill><patternFill patternType="gray125"/></fill>');
+  results.push("</fills>");
+  results.push('<borders count="1">');
+  results.push("<border><left/><right/><top/><bottom/><diagonal/></border>");
+  results.push("</borders>");
+  results.push(
+    '<cellStyleXfs count="1"><xf numFmtId="0" fontId="0" fillId="0" borderId="0"/></cellStyleXfs>'
+  );
+  results.push(
+    '<cellXfs count="1"><xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0"/></cellXfs>'
+  );
+  results.push(
+    '<cellStyles count="1"><cellStyle name="標準" xfId="0" builtinId="0"/></cellStyles><dxfs count="0"/>'
+  );
+  results.push(
+    '<tableStyles count="0" defaultTableStyle="TableStyleMedium2" defaultPivotStyle="PivotStyleMedium9"/>'
+  );
+  results.push("<extLst>");
+  results.push(
+    '<ext uri="{EB79DEF2-80B8-43e5-95BD-54CBDDF9020C}" xmlns:x14="http://schemas.microsoft.com/office/spreadsheetml/2009/9/main">'
+  );
+  results.push(
+    '<x14:slicerStyles defaultSlicerStyle="SlicerStyleLight1"/></ext>'
+  );
+  results.push(
+    '<ext uri="{9260A510-F301-46a8-8635-F512D64BE5F5}" xmlns:x15="http://schemas.microsoft.com/office/spreadsheetml/2010/11/main">'
+  );
+  results.push(
+    '<x15:timelineStyles defaultTimelineStyle="TimeSlicerStyleLight1"/></ext>'
+  );
+  results.push("</extLst>");
+  results.push("</styleSheet>");
   return results.join("");
 }

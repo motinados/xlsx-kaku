381c7a22b8c10e0a4008f221da1253b8fba8d2dd
motinados <motinados@gmail.com>
2023-11-30 06:08:52 +0900

    Fills.makeXml

--- src/fills.ts
+++ src/fills.ts
@@ -12,6 +12,20 @@ type Fill =
       fgColor: string;
     };
 
+// <fills count="3">
+//     <fill>
+//         <patternFill patternType="none"/>
+//     </fill>
+//     <fill>
+//         <patternFill patternType="gray125"/>
+//     </fill>
+//     <fill>
+//         <patternFill patternType="solid">
+//             <fgColor rgb="FFFFFF00"/>
+//             <bgColor indexed="64"/>
+//         </patternFill>
+//     </fill>
+// </fills>
 export class Fills {
   private fills = new Map<string, number>([
     [
@@ -38,5 +52,25 @@ export class Fills {
     const fillId = this.fills.size;
     this.fills.set(key, fillId);
     return fillId;
+  }
+
+  makeXml(): string {
+    let xml = `<fills count="${this.fills.size}">`;
+    this.fills.forEach((_, key) => {
+      const fill = JSON.parse(key) as Fill;
+
+      xml += "<fill>";
+      if (fill.patternType === "none" || fill.patternType === "gray125") {
+        xml += `<patternFill patternType="${fill.patternType}"/>`;
+      } else {
+        xml += `<patternFill patternType="${fill.patternType}">`;
+        xml += `<fgColor rgb="${fill.fgColor}"/>`;
+        xml += '<bgColor indexed="64"/>';
+        xml += "</patternFill>";
+      }
+      xml += "</fill>";
+    });
+    xml += "</fills>";
+    return xml;
   }
 }
--- tests/fillts.test.ts
+++ tests/fillts.test.ts
@@ -12,4 +12,16 @@ describe("Fills", () => {
       3
     );
   });
+
+  test("makeXml", () => {
+    const fills = new Fills();
+    expect(fills.makeXml()).toBe(
+      `<fills count="2"><fill><patternFill patternType="none"/></fill><fill><patternFill patternType="gray125"/></fill></fills>`
+    );
+
+    fills.getFillId({ patternType: "solid", fgColor: "FF0000" });
+    expect(fills.makeXml()).toBe(
+      `<fills count="3"><fill><patternFill patternType="none"/></fill><fill><patternFill patternType="gray125"/></fill><fill><patternFill patternType="solid"><fgColor rgb="FF0000"/><bgColor indexed="64"/></patternFill></fill></fills>`
+    );
+  });
 });

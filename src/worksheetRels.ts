d8b7dcf9f495cee8441650e5f08ce344846c7afa
motinados <69296148+motinados@users.noreply.github.com>
2023-12-07 17:12:01 +0900

    Test: add test for Hyperlinks (#6)

--- src/worksheetRels.ts
+++ src/worksheetRels.ts
@@ -20,6 +20,10 @@ export class WorksheetRels {
     return id;
   }
 
+  getWorksheetRels(): WorksheetRel[] {
+    return this.rels;
+  }
+
   // <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
   //     <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink" Target="http://www.google.com" TargetMode="External"/>
   // </Relationships>

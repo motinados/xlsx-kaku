f80572a3c958046f8b9475495a90e1bfe2d941a1
motinados <motinados@gmail.com>
2023-12-09 16:15:45 +0900

    fix: makeSharedStringsXml

--- src/writer.ts
+++ src/writer.ts
@@ -233,7 +233,8 @@ export function makeSharedStringsXml(sharedStrings: SharedStrings) {
     return null;
   }
 
-  let result = `<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="${sharedStrings.count}" uniqueCount="${sharedStrings.uniqueCount}">`;
+  let result = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+  result += `<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="${sharedStrings.count}" uniqueCount="${sharedStrings.uniqueCount}">`;
   for (const str of sharedStrings.getValuesInOrder()) {
     result += `<si><t>${str}</t></si>`;
   }

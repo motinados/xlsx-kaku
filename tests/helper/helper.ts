2d3d9e736410cb4bce807b165742c4db49f75c3e
motinados <69296148+motinados@users.noreply.github.com>
2023-12-26 05:37:33 +0900

    test: Improve tests (#11)
    * add fast-xml-parser and fflate
    
    * add unzip
    
    * fix: unzip
    
    * fix: unzip size0 files
    
    * add test
    
    * fix: unzip
    
    * add xlsx file for number test
    
    * compare directory file names
    
    * refactor: rename
    
    * deletePropertyFromObject function
    
    * Comparison of styles.xml
    
    * add comments
    
    * comparison of WorkbookXmlRels
    
    * refactor
    
    * refactor
    
    * refactor
    
    * comparison of WorkbookXml
    
    * change xlsx
    
    * comparison of worksheet
    
    * fix: workbookXml test
    
    * refactor

---
+++ tests/helper/helper.ts
@@ -0,0 +1,99 @@
+import * as fflate from "fflate";
+import { readFile, mkdir, writeFileSync, readdirSync, statSync } from "node:fs";
+import { dirname, join, normalize } from "node:path";
+
+function createDirectory(dir: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    mkdir(dir, { recursive: true }, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+export function unzip(xlsxFilePath: string, outputDir: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    readFile(xlsxFilePath, (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      fflate.unzip(new Uint8Array(data), async (unzipErr, unzipped) => {
+        if (unzipErr) {
+          reject(unzipErr);
+          return;
+        }
+
+        for (const [filename, fileContent] of Object.entries(unzipped)) {
+          try {
+            const filepath = join(outputDir, filename);
+            const fileDir = dirname(filepath);
+            await createDirectory(fileDir);
+
+            // When unzipping a file created with xlsx-kaku, some filenames with size 0 are included.
+            if (fileContent.length > 0) {
+              writeFileSync(filepath, Buffer.from(fileContent));
+            }
+          } catch (dirErr) {
+            console.error(dirErr);
+          }
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+export function listFiles(dir: string) {
+  let fileList: string[] = [];
+
+  const files = readdirSync(dir);
+  files.forEach((file) => {
+    const filePath = join(dir, file);
+    const stat = statSync(filePath);
+
+    if (stat.isDirectory()) {
+      // ディレクトリの場合、再帰的に中のファイルをリストに追加
+      fileList = fileList.concat(listFiles(filePath));
+    } else {
+      // ファイルの場合、ファイルパスをリストに追加
+      fileList.push(filePath);
+    }
+  });
+
+  return fileList;
+}
+
+export function removeBasePath(fullPath: string, basePath: string): string {
+  const normalizedFullPath = normalize(fullPath);
+  const normalizedBasePath = normalize(basePath);
+
+  if (normalizedFullPath.startsWith(normalizedBasePath)) {
+    return normalizedFullPath.substring(normalizedBasePath.length);
+  }
+
+  return fullPath;
+}
+
+export function deletePropertyFromObject(obj: any, propertyPath: string): void {
+  const pathElements = propertyPath.split(".");
+
+  let currentObj = obj;
+  for (let i = 0; i < pathElements.length - 1; i++) {
+    const key = pathElements[i];
+    if (key) {
+      currentObj = currentObj[key];
+    }
+  }
+
+  const lastKey = pathElements[pathElements.length - 1];
+  if (lastKey) {
+    if (lastKey in currentObj) {
+      delete currentObj[lastKey];
+    }
+  }
+}
---
+++ tests/number.test.ts
@@ -0,0 +1,173 @@
+import path, { basename, extname } from "node:path";
+import {
+  deletePropertyFromObject,
+  listFiles,
+  removeBasePath,
+  unzip,
+} from "./helper/helper";
+import { Workbook } from "../src/index";
+import { readFileSync, rmSync } from "node:fs";
+import { XMLParser } from "fast-xml-parser";
+
+const OUTPUT_DIR = "tests/output";
+const XLSX_Dir = "tests/xlsx";
+const EXPECTED_UNZIPPED_DIR = "tests/expected";
+const ACTUAL_UNZIPPED_DIR = "tests/actuall";
+
+const parser = new XMLParser({ ignoreAttributes: false });
+
+describe("number", () => {
+  let xlsxBaseName: string;
+  let expectedFileDir: string;
+  let actualFileDir: string;
+  let outputPath: string;
+
+  beforeAll(async () => {
+    const filepath = path.resolve(XLSX_Dir, "number.xlsx");
+
+    const extension = extname(filepath);
+    xlsxBaseName = basename(filepath, extension);
+    expectedFileDir = path.resolve(EXPECTED_UNZIPPED_DIR, xlsxBaseName);
+    await unzip(filepath, expectedFileDir);
+
+    const wb = new Workbook();
+    const ws = wb.addWorksheet("Sheet1");
+    ws.setCell(0, 0, { type: "number", value: 1 });
+    ws.setCell(0, 1, { type: "number", value: 2 });
+    ws.setCell(1, 0, { type: "number", value: 3 });
+    ws.setCell(1, 1, { type: "number", value: 4 });
+    outputPath = path.resolve(OUTPUT_DIR, "number.xlsx");
+    await wb.save(outputPath);
+
+    actualFileDir = path.resolve(ACTUAL_UNZIPPED_DIR, xlsxBaseName);
+    await unzip(outputPath, actualFileDir);
+  });
+
+  afterAll(() => {
+    rmSync(OUTPUT_DIR, { recursive: true });
+    rmSync(EXPECTED_UNZIPPED_DIR, { recursive: true });
+    rmSync(ACTUAL_UNZIPPED_DIR, { recursive: true });
+  });
+
+  test("compare files", async () => {
+    const expectedFiles = listFiles(expectedFileDir);
+    const actualFiles = listFiles(actualFileDir);
+
+    const expectedSubPaths = expectedFiles.map((it) =>
+      removeBasePath(it, expectedFileDir)
+    );
+    const actualSubPaths = actualFiles.map((it) =>
+      removeBasePath(it, actualFileDir)
+    );
+
+    expect(actualSubPaths).toEqual(expectedSubPaths);
+  });
+
+  test("compare StylesXml", () => {
+    const expectedXmlPath = path.resolve(expectedFileDir, "xl/styles.xml");
+    const expectedXml = readFileSync(expectedXmlPath, "utf8");
+
+    const expectedObj = parser.parse(expectedXml);
+    // Differences due to the default font
+    deletePropertyFromObject(expectedObj, "styleSheet.fonts");
+    // It should be a problem-free difference.
+    deletePropertyFromObject(expectedObj, "styleSheet.dxfs");
+
+    const actualXmlPath = path.resolve(actualFileDir, "xl/styles.xml");
+    const actualXml = readFileSync(actualXmlPath, "utf8");
+    const actualObj = parser.parse(actualXml);
+    // Differences due to the default font
+    deletePropertyFromObject(actualObj, "styleSheet.fonts");
+    // It should be a problem-free difference.
+    deletePropertyFromObject(actualObj, "styleSheet.cellStyleXfs.xf.@_xfId");
+
+    expect(actualObj).toEqual(expectedObj);
+  });
+
+  test("compare WorkbookXmlRels", () => {
+    function sortById(a: any, b: any) {
+      const rIdA = parseInt(a["@_Id"].substring(3));
+      const rIdB = parseInt(b["@_Id"].substring(3));
+      if (rIdA < rIdB) {
+        return -1;
+      }
+      if (rIdA > rIdB) {
+        return 1;
+      }
+      return 0;
+    }
+
+    const expectedRelsPath = path.resolve(
+      expectedFileDir,
+      "xl/_rels/workbook.xml.rels"
+    );
+    const expectedRels = readFileSync(expectedRelsPath, "utf8");
+    const actualRelsPath = path.resolve(
+      actualFileDir,
+      "xl/_rels/workbook.xml.rels"
+    );
+    const actualRels = readFileSync(actualRelsPath, "utf8");
+
+    const expectedObj = parser.parse(expectedRels);
+    const actualObj = parser.parse(actualRels);
+
+    const expectedRelationships = expectedObj.Relationships.Relationship;
+    expectedRelationships.sort(sortById);
+
+    const actualRelationships = actualObj.Relationships.Relationship;
+    actualRelationships.sort(sortById);
+
+    expect(actualRelationships).toEqual(expectedRelationships);
+  });
+
+  test("compare WorkbookXml", () => {
+    const expectedXmlPath = path.resolve(expectedFileDir, "xl/workbook.xml");
+    const expectedXml = readFileSync(expectedXmlPath, "utf8");
+    const actualXmlPath = path.resolve(actualFileDir, "xl/workbook.xml");
+    const actualXml = readFileSync(actualXmlPath, "utf8");
+
+    const expectedObj = parser.parse(expectedXml);
+    const actualObj = parser.parse(actualXml);
+
+    // It should be a problem-free difference.
+    deletePropertyFromObject(expectedObj, "workbook.fileVersion.@_rupBuild");
+    deletePropertyFromObject(actualObj, "workbook.fileVersion.@_rupBuild");
+
+    // It should be a problem-free difference.
+    deletePropertyFromObject(
+      expectedObj,
+      "workbook.xr:revisionPtr.@_documentId"
+    );
+    deletePropertyFromObject(actualObj, "workbook.xr:revisionPtr.@_documentId");
+
+    expect(actualObj).toEqual(expectedObj);
+  });
+
+  test("compare worksheets", () => {
+    const expectedXmlPath = path.resolve(
+      expectedFileDir,
+      "xl/worksheets/sheet1.xml"
+    );
+    const expectedXml = readFileSync(expectedXmlPath, "utf8");
+    const actualXmlPath = path.resolve(
+      actualFileDir,
+      "xl/worksheets/sheet1.xml"
+    );
+    const actualXml = readFileSync(actualXmlPath, "utf8");
+
+    const expectedObj = parser.parse(expectedXml);
+    const actualObj = parser.parse(actualXml);
+
+    // It should be a problem-free difference.
+    deletePropertyFromObject(
+      expectedObj,
+      "worksheet.sheetViews.sheetView.selection"
+    );
+    deletePropertyFromObject(
+      actualObj,
+      "worksheet.sheetViews.sheetView.selection"
+    );
+
+    expect(actualObj).toEqual(expectedObj);
+  });
+});

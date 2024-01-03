174481e342b5434aa1e5f39619404370a8316a31
motinados <motinados@gmail.com>
2024-01-03 21:01:01 +0900

    fix: XML closing tag in makeAppXml function

--- src/writer.ts
+++ src/writer.ts
@@ -817,7 +817,7 @@ function makeWorkbookXml(worksheets: Worksheet[]) {
 function makeAppXml() {
   return (
     '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
-    '<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"' +
+    '<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">' +
     "<Application>xlsx-kaku</Application>" +
     "<Manager></Manager>" +
     "<Company></Company>" +

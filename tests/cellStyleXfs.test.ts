b590f6b5a2c378e1ea1c87d6e8d10a8c509ff272
motinados <motinados@gmail.com>
2024-01-12 05:33:50 +0900

    test: add tests

--- tests/cellStyleXfs.test.ts
+++ tests/cellStyleXfs.test.ts
@@ -36,4 +36,69 @@ describe("cellStyleXfs", () => {
       })
     ).toBe(0);
   });
+
+  test("makeXml", () => {
+    const styles = new CellStyleXfs();
+    expect(styles.makeXml()).toBe(
+      '<cellStyleXfs count="1"><xf numFmtId="0" fontId="0" fillId="0" borderId="0"/></cellStyleXfs>'
+    );
+
+    styles.getCellStyleXfId({
+      fillId: 0,
+      fontId: 1,
+      borderId: 0,
+      numFmtId: 0,
+    });
+    expect(styles.makeXml()).toBe(
+      '<cellStyleXfs count="2">' +
+        '<xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>' +
+        '<xf numFmtId="0" fontId="1" fillId="0" borderId="0" applyFont="1"/>' +
+        "</cellStyleXfs>"
+    );
+
+    styles.getCellStyleXfId({
+      fillId: 0,
+      fontId: 0,
+      borderId: 1,
+      numFmtId: 0,
+    });
+    expect(styles.makeXml()).toBe(
+      '<cellStyleXfs count="3">' +
+        '<xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>' +
+        '<xf numFmtId="0" fontId="1" fillId="0" borderId="0" applyFont="1"/>' +
+        '<xf numFmtId="0" fontId="0" fillId="0" borderId="1" applyBorder="1"/>' +
+        "</cellStyleXfs>"
+    );
+
+    styles.getCellStyleXfId({
+      fillId: 0,
+      fontId: 0,
+      borderId: 0,
+      numFmtId: 1,
+    });
+    expect(styles.makeXml()).toBe(
+      '<cellStyleXfs count="4">' +
+        '<xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>' +
+        '<xf numFmtId="0" fontId="1" fillId="0" borderId="0" applyFont="1"/>' +
+        '<xf numFmtId="0" fontId="0" fillId="0" borderId="1" applyBorder="1"/>' +
+        '<xf numFmtId="1" fontId="0" fillId="0" borderId="0" applyNumberFormat="1"/>' +
+        "</cellStyleXfs>"
+    );
+
+    styles.getCellStyleXfId({
+      fillId: 1,
+      fontId: 0,
+      borderId: 0,
+      numFmtId: 0,
+    });
+    expect(styles.makeXml()).toBe(
+      '<cellStyleXfs count="5">' +
+        '<xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>' +
+        '<xf numFmtId="0" fontId="1" fillId="0" borderId="0" applyFont="1"/>' +
+        '<xf numFmtId="0" fontId="0" fillId="0" borderId="1" applyBorder="1"/>' +
+        '<xf numFmtId="1" fontId="0" fillId="0" borderId="0" applyNumberFormat="1"/>' +
+        '<xf numFmtId="0" fontId="0" fillId="1" borderId="0" applyFill="1"/>' +
+        "</cellStyleXfs>"
+    );
+  });
 });

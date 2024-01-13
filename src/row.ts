5bbf9cf5810dc12e41a72318d4f276d1b8af582d
motinados <motinados@gmail.com>
2024-01-13 09:21:46 +0900

    Add combineRowProps function

--- src/row.ts
+++ src/row.ts
@@ -11,3 +11,37 @@ export type RowStyle = {
 };
 
 export type Row = RowHeight | RowStyle;
+
+export type CombinedRow = {
+  index: number;
+  height?: number;
+  style?: CellStyle;
+};
+
+export function combineRowProps(rows: Row[]): CombinedRow[] {
+  const combinedRows: CombinedRow[] = [];
+  for (const row of rows) {
+    const found = combinedRows.find((r) => r.index === row.index);
+    if (found) {
+      if ("height" in row) {
+        found.height = row.height;
+      } else if ("style" in row) {
+        found.style = row.style;
+      }
+      continue;
+    }
+
+    const newCombinedRow: CombinedRow = {
+      index: row.index,
+    };
+    if ("height" in row) {
+      newCombinedRow.height = row.height;
+    }
+    if ("style" in row) {
+      newCombinedRow.style = row.style;
+    }
+    combinedRows.push(newCombinedRow);
+  }
+
+  return combinedRows;
+}

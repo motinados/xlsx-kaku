183ef460cd650df8a3eb83ec6f528ada1ff165bc
motinados <69296148+motinados@users.noreply.github.com>
2024-01-19 05:39:35 +0900

    feat: email hyperlink (#50)
    * Add support for email hyperlinks
    
    * Add support for email hyperlinks
    
    * Add integration test

--- tests/hyperlinks.test.ts
+++ tests/hyperlinks.test.ts
@@ -66,6 +66,41 @@ describe("hyperlinks", () => {
     );
   });
 
+  test("hyperlink with a linkType of 'email'", () => {
+    const hyperlinks = new Hyperlinks();
+    hyperlinks.addHyperlink({
+      linkType: "email",
+      ref: "A1",
+      rid: "rId1",
+      uuid: "00000000-0000-0000-0000-000000000000",
+    });
+    hyperlinks.addHyperlink({
+      linkType: "email",
+      ref: "A2",
+      rid: "rId2",
+      uuid: "00000000-0000-0000-0000-000000000001",
+    });
+
+    expect(hyperlinks.getHyperlinks()).toEqual([
+      {
+        linkType: "email",
+        ref: "A1",
+        rid: "rId1",
+        uuid: "00000000-0000-0000-0000-000000000000",
+      },
+      {
+        linkType: "email",
+        ref: "A2",
+        rid: "rId2",
+        uuid: "00000000-0000-0000-0000-000000000001",
+      },
+    ]);
+
+    expect(hyperlinks.makeXML()).toEqual(
+      '<hyperlinks><hyperlink ref="A1" r:id="rId1" xr:uid="{00000000-0000-0000-0000-000000000000}"/><hyperlink ref="A2" r:id="rId2" xr:uid="{00000000-0000-0000-0000-000000000001}"/></hyperlinks>'
+    );
+  });
+
   test("hyperlinks", () => {
     const hyperlinks = new Hyperlinks();
     hyperlinks.addHyperlink({

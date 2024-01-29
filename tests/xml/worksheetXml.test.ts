1c23b7a2fe43862d9134d3efe3eadc795560a176
motinados <motinados@gmail.com>
2024-01-30 06:02:40 +0900

    test: update worksheetXml tests

--- tests/xml/worksheetXml.test.ts
+++ tests/xml/worksheetXml.test.ts
@@ -17,6 +17,10 @@ import {
   rowToString,
   convRowToXlsxRow,
   makeSheetFormatPrXml,
+  groupXlsxCol,
+  XlsxCol,
+  isEqualsXlsxCol,
+  GroupedXlsxCol,
 } from "../../src/xml/worksheetXml";
 import { CellXfs } from "../../src/cellXfs";
 import { Fonts } from "../../src/fonts";
@@ -28,7 +32,7 @@ import { CellStyles } from "../../src/cellStyles";
 import { Hyperlinks } from "../../src/hyperlinks";
 import { WorksheetRels } from "../../src/worksheetRels";
 import { FreezePane, MergeCell } from "../../src/worksheet";
-import { Col, DEFAULT_COL_WIDTH, combineColProps } from "../../src/col";
+import { ColProps, DEFAULT_COL_WIDTH } from "../../src/col";
 import { DEFAULT_ROW_HEIGHT } from "../../src/row";
 
 describe("Writer", () => {
@@ -137,7 +141,7 @@ describe("Writer", () => {
       value: 15,
     };
     const result = makeCellXml(
-      convertCellToXlsxCell(cell, 2, 0, styleMappers, [], [])
+      convertCellToXlsxCell(cell, 2, 0, styleMappers, new Map(), [])
     );
     expect(result).toBe(`<c r="C1"><v>15</v></c>`);
   });
@@ -149,7 +153,7 @@ describe("Writer", () => {
       value: "hello",
     };
     const result = makeCellXml(
-      convertCellToXlsxCell(cell, 2, 0, styleMappers, [], [])
+      convertCellToXlsxCell(cell, 2, 0, styleMappers, new Map(), [])
     );
     expect(result).toBe(`<c r="C1" t="s"><v>0</v></c>`);
     expect(styleMappers.sharedStrings.count).toBe(1);
@@ -163,7 +167,7 @@ describe("Writer", () => {
       value: "2020-01-01T00:00:00.000Z",
     };
     const result = makeCellXml(
-      convertCellToXlsxCell(cell, 2, 0, styleMappers, [], [])
+      convertCellToXlsxCell(cell, 2, 0, styleMappers, new Map(), [])
     );
     expect(result).toBe(`<c r="C1" s="1"><v>43831</v></c>`);
   });
@@ -177,7 +181,7 @@ describe("Writer", () => {
       linkType: "external",
     };
     const result = makeCellXml(
-      convertCellToXlsxCell(cell, 2, 0, styleMappers, [], [])
+      convertCellToXlsxCell(cell, 2, 0, styleMappers, new Map(), [])
     );
     expect(result).toBe(`<c r="C1" s="1" t="s"><v>0</v></c>`);
 
@@ -215,7 +219,7 @@ describe("Writer", () => {
       { type: "number", value: 15 },
       { type: "number", value: 23 },
     ];
-    const result = rowToString(row, 0, 3, 4, styleMappers, [], []);
+    const result = rowToString(row, 0, 3, 4, styleMappers, new Map(), []);
     expect(result).toBe(
       `<row r="1" spans="3:4"><c r="C1"><v>15</v></c><c r="D1"><v>23</v></c></row>`
     );
@@ -230,7 +234,7 @@ describe("Writer", () => {
       { type: "string", value: "world" },
       { type: "string", value: "hello" },
     ];
-    const result = rowToString(row, 0, 3, 5, styleMappers, [], []);
+    const result = rowToString(row, 0, 3, 5, styleMappers, new Map(), []);
     expect(result).toBe(
       `<row r="1" spans="3:5"><c r="C1" t="s"><v>0</v></c><c r="D1" t="s"><v>1</v></c><c r="E1" t="s"><v>0</v></c></row>`
     );
@@ -241,15 +245,9 @@ describe("Writer", () => {
   test("rowToString with height", () => {
     const styleMappers = getStyleMappers();
     const row: RowData = [{ type: "number", value: 10 }];
-    const result = rowToString(
-      row,
-      0,
-      1,
-      1,
-      styleMappers,
-      [],
-      [convRowToXlsxRow({ index: 0, height: 30 }, styleMappers)]
-    );
+    const result = rowToString(row, 0, 1, 1, styleMappers, new Map(), [
+      convRowToXlsxRow({ index: 0, height: 30 }, styleMappers),
+    ]);
     expect(result).toBe(
       `<row r="1" spans="1:1" ht="30" customHeight="1"><c r="A1"><v>10</v></c></row>`
     );
@@ -258,20 +256,12 @@ describe("Writer", () => {
   test("rowToString with style", () => {
     const styleMappers = getStyleMappers();
     const row: RowData = [{ type: "number", value: 10 }];
-    const result = rowToString(
-      row,
-      0,
-      1,
-      1,
-      styleMappers,
-      [],
-      [
-        convRowToXlsxRow(
-          { index: 0, style: { alignment: { horizontal: "center" } } },
-          styleMappers
-        ),
-      ]
-    );
+    const result = rowToString(row, 0, 1, 1, styleMappers, new Map(), [
+      convRowToXlsxRow(
+        { index: 0, style: { alignment: { horizontal: "center" } } },
+        styleMappers
+      ),
+    ]);
     expect(result).toBe(
       `<row r="1" spans="1:1" s="1" customFormat="1"><c r="A1" s="1"><v>10</v></c></row>`
     );
@@ -280,24 +270,16 @@ describe("Writer", () => {
   test("rowToString with style and height", () => {
     const styleMappers = getStyleMappers();
     const row: RowData = [{ type: "number", value: 10 }];
-    const result = rowToString(
-      row,
-      0,
-      1,
-      1,
-      styleMappers,
-      [],
-      [
-        convRowToXlsxRow(
-          {
-            index: 0,
-            height: 30,
-            style: { alignment: { horizontal: "center" } },
-          },
-          styleMappers
-        ),
-      ]
-    );
+    const result = rowToString(row, 0, 1, 1, styleMappers, new Map(), [
+      convRowToXlsxRow(
+        {
+          index: 0,
+          height: 30,
+          style: { alignment: { horizontal: "center" } },
+        },
+        styleMappers
+      ),
+    ]);
     expect(result).toBe(
       `<row r="1" spans="1:1" s="1" customFormat="1" ht="30" customHeight="1"><c r="A1" s="1"><v>10</v></c></row>`
     );
@@ -310,7 +292,12 @@ describe("Writer", () => {
       [{ type: "number", value: 3 }, { type: "number", value: 4 }, null, null],
     ];
     const styleMappers = getStyleMappers();
-    const sheetDataXml = makeSheetDataXml(sheetData, styleMappers, [], []);
+    const sheetDataXml = makeSheetDataXml(
+      sheetData,
+      styleMappers,
+      new Map(),
+      []
+    );
     expect(sheetDataXml).toBe(
       `<sheetData><row r="2" spans="1:4"><c r="C2"><v>1</v></c><c r="D2"><v>2</v></c></row><row r="3" spans="1:4"><c r="A3"><v>3</v></c><c r="B3"><v>4</v></c></row></sheetData>`
     );
@@ -328,7 +315,12 @@ describe("Writer", () => {
       ],
     ];
     const styleMappers = getStyleMappers();
-    const sheetDataXml = makeSheetDataXml(sheetData, styleMappers, [], []);
+    const sheetDataXml = makeSheetDataXml(
+      sheetData,
+      styleMappers,
+      new Map(),
+      []
+    );
     expect(sheetDataXml).toBe(
       `<sheetData><row r="2" spans="1:3"><c r="C2" t="s"><v>0</v></c></row><row r="3" spans="1:3"><c r="A3" t="s"><v>1</v></c><c r="B3" t="s"><v>1</v></c></row></sheetData>`
     );
@@ -336,60 +328,79 @@ describe("Writer", () => {
 
   test("makeColsXml for width", () => {
     const styleMappers = getStyleMappers();
-    const cols: Col[] = [
-      { startIndex: 0, endIndex: 0, width: 10 },
-      { startIndex: 1, endIndex: 1, width: 75 },
-      { startIndex: 2, endIndex: 5, width: 25 },
+    const cols: ColProps[] = [
+      { index: 0, width: 10 },
+      { index: 1, width: 75 },
+      { index: 2, width: 25 },
+      { index: 3, width: 25 },
+      { index: 4, width: 25 },
+      { index: 5, width: 25 },
     ];
 
-    const xlsxCols = combineColProps(cols).map((col) =>
-      convertCombinedColToXlsxCol(col, styleMappers, DEFAULT_COL_WIDTH)
-    );
-    expect(makeColsXml(xlsxCols, DEFAULT_COL_WIDTH)).toBe(
+    const xlsxCols = new Map<number, XlsxCol>();
+    cols.forEach((col) => {
+      xlsxCols.set(
+        col.index,
+        convertCombinedColToXlsxCol(col, styleMappers, DEFAULT_COL_WIDTH)
+      );
+    });
+    const groupedXlsxCols = groupXlsxCol(xlsxCols);
+    expect(makeColsXml(groupedXlsxCols, DEFAULT_COL_WIDTH)).toBe(
       `<cols><col min="1" max="1" width="10" customWidth="1"/><col min="2" max="2" width="75" customWidth="1"/><col min="3" max="6" width="25" customWidth="1"/></cols>`
     );
   });
 
   test("makeColsXml for default width", () => {
     const styleMappers = getStyleMappers();
-    const cols: Col[] = [
-      { startIndex: 0, endIndex: 0, width: DEFAULT_COL_WIDTH },
-    ];
+    const cols: ColProps[] = [{ index: 0, width: DEFAULT_COL_WIDTH }];
 
-    const xlsxCols = combineColProps(cols).map((col) =>
-      convertCombinedColToXlsxCol(col, styleMappers, DEFAULT_COL_WIDTH)
-    );
-    expect(makeColsXml(xlsxCols, DEFAULT_COL_WIDTH)).toBe(
+    const xlsxCols = new Map<number, XlsxCol>();
+    cols.forEach((col) => {
+      xlsxCols.set(
+        col.index,
+        convertCombinedColToXlsxCol(col, styleMappers, DEFAULT_COL_WIDTH)
+      );
+    });
+    const groupedXlsxCols = groupXlsxCol(xlsxCols);
+
+    // TODO: Is this necessary?
+    expect(makeColsXml(groupedXlsxCols, DEFAULT_COL_WIDTH)).toBe(
       `<cols><col min="1" max="1" width="${DEFAULT_COL_WIDTH}"/></cols>`
     );
   });
 
   test("makeColsXml for style", () => {
     const styleMappers = getStyleMappers();
-    const cols: Col[] = [
+    const cols: ColProps[] = [
       {
-        startIndex: 0,
-        endIndex: 0,
+        index: 0,
         style: { alignment: { horizontal: "center" } },
       },
       {
-        startIndex: 1,
-        endIndex: 2,
+        index: 1,
+        width: 25,
         style: {
           fill: { patternType: "solid", fgColor: "FFFF0000" },
         },
       },
       {
-        startIndex: 1,
-        endIndex: 2,
+        index: 2,
         width: 25,
+        style: {
+          fill: { patternType: "solid", fgColor: "FFFF0000" },
+        },
       },
     ];
 
-    const xlsxCols = combineColProps(cols).map((col) =>
-      convertCombinedColToXlsxCol(col, styleMappers, DEFAULT_COL_WIDTH)
-    );
-    expect(makeColsXml(xlsxCols, DEFAULT_COL_WIDTH)).toBe(
+    const xlsxCols = new Map<number, XlsxCol>();
+    cols.forEach((col) => {
+      xlsxCols.set(
+        col.index,
+        convertCombinedColToXlsxCol(col, styleMappers, DEFAULT_COL_WIDTH)
+      );
+    });
+    const groupedXlsxCols = groupXlsxCol(xlsxCols);
+    expect(makeColsXml(groupedXlsxCols, DEFAULT_COL_WIDTH)).toBe(
       `<cols><col min="1" max="1" width="${DEFAULT_COL_WIDTH}" style="1"/><col min="2" max="3" width="25" customWidth="1" style="2"/></cols>`
     );
   });
@@ -448,5 +459,399 @@ describe("Writer", () => {
     expect(makeSheetFormatPrXml(DEFAULT_ROW_HEIGHT, DEFAULT_COL_WIDTH)).toBe(
       `<sheetFormatPr defaultRowHeight="${DEFAULT_ROW_HEIGHT}"/>`
     );
+  });
+
+  test("isEqualsXlsxCol", () => {
+    const col0: XlsxCol = {
+      index: 0,
+      width: DEFAULT_COL_WIDTH,
+      customWidth: false,
+      cellXfId: 1,
+    };
+    const col1: XlsxCol = {
+      index: 1,
+      width: 25,
+      customWidth: true,
+      cellXfId: 2,
+    };
+    const col2: XlsxCol = {
+      index: 1,
+      width: 25,
+      customWidth: true,
+      cellXfId: 2,
+    };
+    expect(isEqualsXlsxCol(col0, col1)).toBe(false);
+    expect(isEqualsXlsxCol(col1, col2)).toBe(true);
+  });
+
+  test("groupXlsxCol 1", () => {
+    // a, b
+    const c1: XlsxCol = {
+      index: 0,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c2: XlsxCol = {
+      index: 1,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const actual = groupXlsxCol(
+      new Map([
+        [0, c1],
+        [1, c2],
+      ])
+    );
+    const expected: GroupedXlsxCol[] = [
+      {
+        startIndex: 0,
+        endIndex: 0,
+        width: 10,
+        cellXfId: 0,
+        customWidth: true,
+      },
+      {
+        startIndex: 1,
+        endIndex: 1,
+        width: 20,
+        cellXfId: 1,
+        customWidth: true,
+      },
+    ];
+    expect(actual).toEqual(expected);
+  });
+
+  test("groupXlsxCol 2", () => {
+    // a, b, b
+    const c1: XlsxCol = {
+      index: 0,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c2: XlsxCol = {
+      index: 1,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const c3: XlsxCol = {
+      index: 2,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const actual = groupXlsxCol(
+      new Map([
+        [0, c1],
+        [1, c2],
+        [2, c3],
+      ])
+    );
+    const expected: GroupedXlsxCol[] = [
+      {
+        startIndex: 0,
+        endIndex: 0,
+        width: 10,
+        cellXfId: 0,
+        customWidth: true,
+      },
+      {
+        startIndex: 1,
+        endIndex: 2,
+        width: 20,
+        cellXfId: 1,
+        customWidth: true,
+      },
+    ];
+    expect(actual).toEqual(expected);
+  });
+
+  test("groupXlsxCol 3", () => {
+    // a, a, b
+    const c1: XlsxCol = {
+      index: 0,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c2: XlsxCol = {
+      index: 1,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c3: XlsxCol = {
+      index: 2,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const actual = groupXlsxCol(
+      new Map([
+        [0, c1],
+        [1, c2],
+        [2, c3],
+      ])
+    );
+    const expected: GroupedXlsxCol[] = [
+      {
+        startIndex: 0,
+        endIndex: 1,
+        width: 10,
+        cellXfId: 0,
+        customWidth: true,
+      },
+      {
+        startIndex: 2,
+        endIndex: 2,
+        width: 20,
+        cellXfId: 1,
+        customWidth: true,
+      },
+    ];
+    expect(actual).toEqual(expected);
+  });
+
+  test("groupXlsxCol 4", () => {
+    // a, a, a, b
+    const c1: XlsxCol = {
+      index: 0,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c2: XlsxCol = {
+      index: 1,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c3: XlsxCol = {
+      index: 2,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c4: XlsxCol = {
+      index: 3,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const actual = groupXlsxCol(
+      new Map([
+        [0, c1],
+        [1, c2],
+        [2, c3],
+        [3, c4],
+      ])
+    );
+    const expected: GroupedXlsxCol[] = [
+      {
+        startIndex: 0,
+        endIndex: 2,
+        width: 10,
+        cellXfId: 0,
+        customWidth: true,
+      },
+      {
+        startIndex: 3,
+        endIndex: 3,
+        width: 20,
+        cellXfId: 1,
+        customWidth: true,
+      },
+    ];
+    expect(actual).toEqual(expected);
+  });
+
+  test("groupXlsxCol 5", () => {
+    // a, b, b, b
+    const c1: XlsxCol = {
+      index: 0,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c2: XlsxCol = {
+      index: 1,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const c3: XlsxCol = {
+      index: 2,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const c4: XlsxCol = {
+      index: 3,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const actual = groupXlsxCol(
+      new Map([
+        [0, c1],
+        [1, c2],
+        [2, c3],
+        [3, c4],
+      ])
+    );
+    const expected: GroupedXlsxCol[] = [
+      {
+        startIndex: 0,
+        endIndex: 0,
+        width: 10,
+        cellXfId: 0,
+        customWidth: true,
+      },
+      {
+        startIndex: 1,
+        endIndex: 3,
+        width: 20,
+        cellXfId: 1,
+        customWidth: true,
+      },
+    ];
+    expect(actual).toEqual(expected);
+  });
+
+  test("groupXlsxCol 6", () => {
+    // a, a, a, b, a
+    const c1: XlsxCol = {
+      index: 0,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c2: XlsxCol = {
+      index: 1,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c3: XlsxCol = {
+      index: 2,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c4: XlsxCol = {
+      index: 3,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const c5: XlsxCol = {
+      index: 4,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const actual = groupXlsxCol(
+      new Map([
+        [0, c1],
+        [1, c2],
+        [2, c3],
+        [3, c4],
+        [4, c5],
+      ])
+    );
+    const expected: GroupedXlsxCol[] = [
+      {
+        startIndex: 0,
+        endIndex: 2,
+        width: 10,
+        cellXfId: 0,
+        customWidth: true,
+      },
+      {
+        startIndex: 3,
+        endIndex: 3,
+        width: 20,
+        cellXfId: 1,
+        customWidth: true,
+      },
+      {
+        startIndex: 4,
+        endIndex: 4,
+        width: 10,
+        cellXfId: 0,
+        customWidth: true,
+      },
+    ];
+    expect(actual).toEqual(expected);
+  });
+
+  test("groupXlsxCol 7", () => {
+    // a, b, b, b, a
+    const c1: XlsxCol = {
+      index: 0,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const c2: XlsxCol = {
+      index: 1,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const c3: XlsxCol = {
+      index: 2,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const c4: XlsxCol = {
+      index: 3,
+      width: 20,
+      cellXfId: 1,
+      customWidth: true,
+    };
+    const c5: XlsxCol = {
+      index: 4,
+      width: 10,
+      cellXfId: 0,
+      customWidth: true,
+    };
+    const actual = groupXlsxCol(
+      new Map([
+        [0, c1],
+        [1, c2],
+        [2, c3],
+        [3, c4],
+        [4, c5],
+      ])
+    );
+    const expected: GroupedXlsxCol[] = [
+      {
+        startIndex: 0,
+        endIndex: 0,
+        width: 10,
+        cellXfId: 0,
+        customWidth: true,
+      },
+      {
+        startIndex: 1,
+        endIndex: 3,
+        width: 20,
+        cellXfId: 1,
+        customWidth: true,
+      },
+      {
+        startIndex: 4,
+        endIndex: 4,
+        width: 10,
+        cellXfId: 0,
+        customWidth: true,
+      },
+    ];
+    expect(actual).toEqual(expected);
   });
 });

5684875bacd45b8c45c79a71968f3a273a942275
motinados <motinados@gmail.com>
2024-03-04 05:39:53 +0900

    Fix tests

--- tests/writer.test.ts
+++ tests/writer.test.ts
@@ -1,24 +1,27 @@
 import { Worksheet } from "../src";
+import { ImageStore } from "../src/imageStore";
 import { genXlsx, genXlsxSync } from "../src/writer";
 
 describe("writer", () => {
   test("genXlsx", async () => {
-    const ws = new Worksheet("Sheet1");
+    const imageStore = new ImageStore();
+    const ws = new Worksheet("Sheet1", imageStore);
     ws.setCell(1, 1, { type: "string", value: "Hello" });
-    const xlsx = await genXlsx([ws]);
+    const xlsx = await genXlsx([ws], imageStore);
     expect(xlsx).toBeInstanceOf(Uint8Array);
   });
 
   test("genXlsxSync", () => {
-    const ws = new Worksheet("Sheet1");
+    const imageStore = new ImageStore();
+    const ws = new Worksheet("Sheet1", imageStore);
     ws.setCell(1, 1, { type: "string", value: "Hello" });
-    const xlsx = genXlsxSync([ws]);
+    const xlsx = genXlsxSync([ws], imageStore);
     expect(xlsx).toBeInstanceOf(Uint8Array);
   });
 
   test("A Error should occur when there is no sheet.", async () => {
     try {
-      await genXlsx([]);
+      await genXlsx([], new ImageStore());
     } catch (e) {
       expect(e).toBeInstanceOf(Error);
     }
@@ -26,7 +29,7 @@ describe("writer", () => {
 
   test("A Error should occur when there is no sheet.", () => {
     try {
-      genXlsxSync([]);
+      genXlsxSync([], new ImageStore());
     } catch (e) {
       expect(e).toBeInstanceOf(Error);
     }

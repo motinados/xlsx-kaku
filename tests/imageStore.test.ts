60407cebe306877440f8b47d271b2245a7afb9df
motinados <69296148+motinados@users.noreply.github.com>
2024-03-06 05:25:15 +0900

    test: Add imageStore tests (#83)

---
+++ tests/imageStore.test.ts
@@ -0,0 +1,44 @@
+import { ImageStore } from "../src/imageStore";
+
+describe("imageStore", () => {
+  test("getHashFn", async () => {
+    const imageStore = new ImageStore();
+    const hashFn = await imageStore.getHashFn();
+    expect(hashFn).toBeDefined();
+  });
+
+  test("addImage", async () => {
+    const imageStore = new ImageStore();
+    const data1 = new Uint8Array([1, 2, 3]);
+    const extension = "png";
+    const fileBasename = await imageStore.addImage(data1, extension);
+    expect(fileBasename).toEqual("image1");
+    expect(imageStore.getAllImages().size).toEqual(1);
+
+    const data2 = new Uint8Array([4, 5, 6]);
+    const fileBasename2 = await imageStore.addImage(data2, extension);
+    expect(fileBasename2).toEqual("image2");
+    expect(imageStore.getAllImages().size).toEqual(2);
+
+    const data3 = new Uint8Array([1, 2, 3]);
+    const fileBasename3 = await imageStore.addImage(data3, extension);
+    expect(fileBasename3).toEqual("image1");
+    expect(imageStore.getAllImages().size).toEqual(2);
+  });
+
+  test("getAllImages", async () => {
+    const imageStore = new ImageStore();
+    const data1 = new Uint8Array([1, 2, 3]);
+    const extension = "png";
+    await imageStore.addImage(data1, extension);
+    expect(imageStore.getAllImages().size).toEqual(1);
+
+    const data2 = new Uint8Array([4, 5, 6]);
+    await imageStore.addImage(data2, extension);
+    expect(imageStore.getAllImages().size).toEqual(2);
+
+    const data3 = new Uint8Array([7, 8, 9]);
+    await imageStore.addImage(data3, extension);
+    expect(imageStore.getAllImages().size).toEqual(3);
+  });
+});

cbf7840023fbbf3d38a81b59e0e616445cc779cd
motinados <69296148+motinados@users.noreply.github.com>
2024-03-23 16:08:56 +0900

    feat: workbook s and worksheet s (#93)
    * Add WorkbookS and WorksheetS classes
    
    * Refactor genXlsx function to accept null imageStore
    
    * Refactor Workbook and Worksheet classes

--- src/workbook.ts
+++ src/workbook.ts
@@ -1,7 +1,15 @@
 import { genXlsx, genXlsxSync } from "./writer";
-import { Worksheet, WorksheetProps, WorksheetType } from "./worksheet";
+import {
+  Worksheet,
+  WorksheetProps,
+  WorksheetS,
+  WorksheetType,
+} from "./worksheet";
 import { ImageStore } from "./imageStore";
 
+/**
+ * Standard Workbook class
+ */
 export class Workbook {
   private _worksheets: WorksheetType[] = [];
   private _imageStore: ImageStore = new ImageStore();
@@ -26,5 +34,34 @@ export class Workbook {
 
   generateXlsx() {
     return genXlsx(this._worksheets, this._imageStore);
+  }
+}
+
+/**
+ * Simplified Workbook class
+ */
+export class WorkbookS {
+  private _worksheets: WorksheetType[] = [];
+
+  addWorksheet(sheetName: string, props?: WorksheetProps) {
+    if (this._worksheets.some((ws) => ws.name === sheetName)) {
+      throw new Error(`Worksheet name "${sheetName}" is already used.`);
+    }
+
+    const ws = new WorksheetS(sheetName, props);
+    this._worksheets.push(ws);
+    return ws;
+  }
+
+  getWorksheet(sheetName: string) {
+    return this._worksheets.find((ws) => ws.name === sheetName);
+  }
+
+  generateXlsxSync() {
+    return genXlsxSync(this._worksheets, null);
+  }
+
+  generateXlsx() {
+    return genXlsx(this._worksheets, null);
   }
 }

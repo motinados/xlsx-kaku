80c87941503e52262e138a2336af77307499935e
motinados <motinados@gmail.com>
2026-02-01 07:17:50 +0900

    fix: add SettableCell and SettableNullableCell exports to index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import { Border } from "./borders";
 import { Fill } from "./fills";
 import { Font } from "./fonts";
 import { NumberFormat } from "./numberFormats";
-import { Cell, NullableCell, RowData, SheetData } from "./sheetData";
+import {
+  Cell,
+  NullableCell,
+  RowData,
+  SheetData,
+  SettableCell,
+  SettableNullableCell,
+} from "./sheetData";
 import { Workbook, WorkbookS } from "./workbook";
 import {
   ColOpts,
@@ -19,7 +26,14 @@ export { Border };
 export { Fill };
 export { Font };
 export { NumberFormat };
-export { Cell, NullableCell, RowData, SheetData };
+export {
+  Cell,
+  NullableCell,
+  RowData,
+  SheetData,
+  SettableCell,
+  SettableNullableCell,
+};
 export { Workbook, WorkbookS };
 export {
   ColOpts,
